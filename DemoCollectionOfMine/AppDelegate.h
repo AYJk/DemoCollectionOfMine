0f172fc81ba4733734db0e568ebff644b5655c92
黄鹏飞 <andy@huyaqundemacintosh.local>
2015-12-28 16:40:25 +0800

    Initial Commit

---
+++ DemoCollectionOfMine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoCollectionOfMine
+//
+//  Created by Andy on 15/12/28.
+//  Copyright © 2015年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoCollectionOfMine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoCollectionOfMine
+//
+//  Created by Andy on 15/12/28.
+//  Copyright © 2015年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
